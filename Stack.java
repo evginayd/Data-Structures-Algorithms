d8d4bee4af09741f5459abfa54f6ef465212f3ba
Evgin Aydın <129039318+evginayd@users.noreply.github.com>
2024-06-08 22:20:46 +0300

    Create Stack.java

---
+++ Stack.java
@@ -0,0 +1,56 @@
+package Stack;
+
+import java.util.Stack;
+
+public class Stack {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Stack<Integer> stack = new Stack<Integer>();
+        push(stack);
+        pop(stack);
+        push(stack);
+        peek(stack);
+        search(stack, 2);
+        search(stack, 6);    
+        
+	}
+	static void push(Stack<Integer> stack) 
+	{
+		for(int i = 0; i < 5; i++) 
+		{
+			stack.push(i);
+		}
+	}
+	
+	static void pop(Stack<Integer> stack) 
+	{
+		System.out.println("Pop operation: ");
+		for(int i = 0; i < 5; i++) 
+		{
+			Integer y = (Integer) stack.pop();
+			System.out.println(y);
+			
+		}
+	}
+	
+	static void peek(Stack<Integer> stack) 
+	{
+		Integer element = (Integer) stack.peek();
+		System.out.println("Element on stack top: " + element);
+	}
+	
+	static void search(Stack<Integer> stack, int element) 
+	{
+		Integer index = (Integer) stack.search(element);
+		
+		if(index == -1) 
+		{
+			System.out.println("Element not found");
+		}
+		else 
+		{
+			System.out.println("Element is found at " + index + "th position");
+		}
+	}
+}
